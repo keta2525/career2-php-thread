5c0cace83ca1eb753f0ac6a8cda36e5bb79e3016
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-06-17 09:32:25 +0900

    adasdaddad

--- index.php
+++ index.php
@@ -102,7 +102,7 @@ function writeData(){
 
     //ブラウザのリロード対策
     //11/09分
-    //11/27分aaa
+    //11/27分aaaaaa
     $redirect_url = $_SERVER['HTTP_REFERER'];
     header("Location: $redirect_url");
     exit;
