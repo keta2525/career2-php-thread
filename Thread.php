088f53a92481a94eee2688ebe53e9e79b61151ee
2019 DSP6101 <2019DSP6101@ntsv6101.com>
2020-07-01 12:25:13 +0900

    2020/7/1/12:24

--- Thread.php
+++ Thread.php
@@ -4,8 +4,7 @@ class Thread {
 
     private $name;
     private const THREAD_FILE = 'thread.txt';
-
-    public function __construct()
+    public function __construct(string $name)
     {
         $this->name = $name;
     }
@@ -14,12 +13,18 @@ class Thread {
         return file_get_contents(self::THREAD_FILE);    
     }
 
-    public function post() {
+    public function post(string $personal_name, string $contents)
+    {
+        $data = "<hr>\n";
+        $data = $data."<p>投稿日時: ".date("Y/m/d H:i:s")."</p>\n";
+        $data = $data."<p>投稿者:".$personal_name."</p>\n";
+        $data = $data."<p>内容:".$contents."</p>\n";
 
+        file_put_contents(self::THREAD_FILE, $data, FILE_APPEND);
     }
 
     public function delete() {
-
+        file_put_contents(self::THREAD_FILE, "");
     }
 }
 
--- index.php
+++ index.php
@@ -1,45 +1,69 @@
- 
- 
-    <html lang="ja">
+<?php
+
+/**
+ * 職業実践2 - 掲示板アプリ
+ */
+
+session_start();
+
+function setToken()
+{
+    $token = sha1(uniqid(mt_rand(), true));
+    $_SESSION['token'] = $token;
+}
+
+function checkToken()
+{
+    if (empty($_SESSION['token'])) {
+        echo "Sessionが空です";
+        exit;
+    }
+
+    if (($_SESSION['token']) !== $_POST['token']) {
+        echo "不正な投稿です。";
+        exit;
+    }
+
+    $_SESSION['token'] = null;
+}
+
+if (empty($_SESSION['token'])) {
+    setToken();
+}
+?>
+
+<html lang="ja">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" rel="stylesheet">
     <link rel="stylesheet" href="example.css">
     <title>掲示板App</title>
+
 </head>
+<body>
 
+<div class="container">
+    <div class="col-md-8">
+        <h1 class="text-center text-primary py-3">掲示板App</h1>
 
-<div class="container .bg_test">
-    <div class="container w-50">
-        <h1 class="text-center text-success py-3">掲示板App(yumoto)</h1>
-        <h2 class="text-muted text-center py-3">投稿フォーム</h2>
-
-        <form  method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-            <div class="form-group">
-                <label for="name">名前</label>
-                <input type="text" class="form-control" id="name" name="personal_name" size="30" placeholder="名前" required><br>
-            </div>
-
-            <div class="form-group">
-                <label for="contents">内容</label>
-                <textarea class="form-control" id="contents" type="text" name="contents" placeholder="内容" rows="5" cols="30" required></textarea>
-            </div>
-
-            <div class="text-center">
-            <button class="btn btn-success w-50" type="submit">投稿する</button>
-            </div>
+        <h2 class="text-muted py-3">投稿フォーム</h2>
+        <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+            <input type="text" class="form-control" name="personal_name" placeholder="名前" required><br><br>
+            <textarea name="contents" class="form-control" rows="8" cols="40" placeholder="内容" required></textarea>
+            <br><br>
+            <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>">
+            <input class="btn btn-primary"  type="submit" name="btn" value="投稿する">
         </form>
 
-<hr>
 
-<h2 class="text-muted py-3">スレッド</h2>
+        <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+            <input type="hidden" name="method" value="DELETE">
+            <button class="btn btn-danger" type="submit">投稿を全削除する</button>
+        </form>
 
-<form method="post" action="delete.php">
-        <button class="btn btn-danger" type ="submit">投稿を全削除する</button>
-    </form>
-
+        <h2 class="text-muted py-3">スレッド</h2>
 <?php
 
 const THREAD_FILE = 'thread.txt';
@@ -47,77 +71,35 @@ const THREAD_FILE = 'thread.txt';
 require_once './Thread.php';
 $thread = new Thread('掲示板App');
 
-if($_SERVER["REQUEST_METHOD"] === "POST"){
-    writeData();
-}
-
-readData();
-
-function readData(){
-    if(! file_exists(THREAD_FILE)){
-        $fp = fopen(THREAD_FILE, 'w');
-        fwrite($fp, '');
-        fclose($fp);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["method"]) && $_POST["method"] === "DELETE") {
+        $thread->delete();
+    } else {
+        $thread->post($_POST['personal_name'], $_POST['contents']);
     }
 
-    $fp = fopen(THREAD_FILE, 'rb');
-
-    if($fp){
-        if(flock($fp, LOCK_SH)){
-            while(!feof($fp)){
-                $buffer = fgets($fp);
-                print($buffer);
-            }
-
-
-            flock($fp, LOCK_UN);
-        }else{
-            print('ファイルのロックに失敗しました');
-        }
-    }
-
-    fclose($fp); 
-}
-
-function writeData(){
-    $personal_name = $_POST['personal_name'];
-    $contents = $_POST['contents'];
-    $contents = nl2br($contents);
-
-    $data = "<hr>\n";
-    $data = $data."<p>投稿日時: ".date("Y/m/d H:i:s")."</p>\n";
-    $data = $data."<p>投稿者 : ".$personal_name."</p>\n";
-    $data = $data."$contents\n";
-
-    $fp = fopen(THREAD_FILE,'ab');
-
-    if($fp){
-        if(flock($fp, LOCK_EX)){
-            if(fwrite($fp, $data) === FALSE){
-                print('ファイル書き込みに失敗しました');
-            }
-
-            flock($fp, LOCK_UN);
-        }else{
-            print('ファイルロックに失敗しました');
-        }
-    }
-
-    fclose($fp);
-
-    //ブラウザのリロード対策
-    //git pull origin master
+    // ブラウザのリロード対策
     $redirect_url = $_SERVER['HTTP_REFERER'];
     header("Location: $redirect_url");
     exit;
 }
 
+echo $thread->getList();
 
 ?>
-</div>
+
     </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</div>
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+</script>
 </body>
 </html>
